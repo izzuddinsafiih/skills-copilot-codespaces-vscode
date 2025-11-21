f194b1c4411f47c132071e58dda6b80855b8219e
Muhammad Izzuddin bin Muhamad Safiih <68729368+izzuddinsafiih@users.noreply.github.com>
2025-11-21 06:37:51 +0000

    Copilot second commit

---
+++ members.js
@@ -0,0 +1,20 @@
+function skillsMember() {
+    return [
+        {
+            skill: "JavaScript",
+            level: "Advanced",
+            yearsExperience: 5
+        },
+        {
+            skill: "Python",
+            level: "Intermediate",
+            yearsExperience: 3
+        },
+        {
+            skill: "HTML/CSS",
+            level: "Advanced",
+            yearsExperience: 6
+        }
+    ];
+}
+module.exports = skillsMember;  
